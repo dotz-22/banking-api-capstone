0f54f24ec0aa94843adc43460f708d5ca8a7b7b5
dotz-22 <babarindesheriff@gmail.com>
2025-03-02 22:15:26 +0100

    change

--- capstone/bankdemo/settings.py
+++ capstone/bankdemo/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'capstone.users',
+    'users',
     'accounts',
     "transactions",
     "support_ticket",
