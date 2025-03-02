bbd6b742fd03d14c970a65e3765a3d82057aad65
dotz-22 <babarindesheriff@gmail.com>
2025-03-02 22:27:18 +0100

    2

--- capstone/support_ticket/apps.py
+++ capstone/support_ticket/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SupportTicketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'support_ticket' 
+    name = 'capstone.support_ticket' 
