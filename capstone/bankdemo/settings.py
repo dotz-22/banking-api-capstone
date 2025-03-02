d1ccdccacca8d10424129a5f04a43bf736526a91
dotz-22 <babarindesheriff@gmail.com>
2025-03-02 15:57:01 +0100

    change

--- capstone/bankdemo/settings.py
+++ capstone/bankdemo/settings.py
@@ -144,7 +144,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL= 'users.NewUser' 
+AUTH_USER_MODEL= 'capstone.users.NewUser' 
 
 
 REST_FRAMEWORK = {
