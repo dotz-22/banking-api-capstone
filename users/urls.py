90bbae3bfe2d59751c7c6f209cdd831098e7d2db
dotz-22 <babarindesheriff@gmail.com>
2025-03-04 15:09:49 +0100

    n

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from .views import UserRegistrationView, UserLoginView, home
 urlpatterns = [ 
     path('register/', UserRegistrationView.as_view(),name='user_registration'),
     path('login/', UserLoginView.as_view(),name='user_login'),
-    path('home/', home.as_view(),name='user_login'),
+    path('', home.as_view(),name='user_login'),
 
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   
